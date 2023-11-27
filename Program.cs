7377d697d855346d264a941331edd89cb3362bfa
lrobinson1723 <151603408+lrobinson1723@users.noreply.github.com>
2023-11-27 11:06:15 -0600

    Add files via upload
    Initial dump of Final Project example

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+namespace FinalProject
+{
+    public class Program
+    {
+        public static List<SteamGames> gameData;
+        public const int GamesPerPage = 47;
+
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Expected exactly one argument, the path to the data file");
+                Environment.Exit(1);
+            }
+
+            string dataFile = "steamProject.csv";
+            gameData = SteamDataLoader.LoadSteamGamesFromCSV(dataFile);
+
+            Console.WriteLine("Welcome to the Steam Game Browser! Find a collection of games and download their purchasing information!");
+
+            while (true)
+            {
+                Console.WriteLine("What would you like to search by? Title, Genre, Price, Tag, Recommended Games");
+                string searchOption = Console.ReadLine().ToUpper();
+
+                if (searchOption == "TITLE")
+                {
+                    Search.SearchByTitle();
+                }
+                else if (searchOption == "GENRE")
+                {
+                    Search.SearchByGenre();
+                }
+                else if (searchOption == "PRICE")
+                {
+                    Search.SearchByPrice();
+                }
+                else if (searchOption == "TAG")
+                {
+                    Search.SearchByTag();
+                }
+                else if (searchOption == "RECOMMENDED GAMES")
+                {
+                    Search.SearchRecommendedGames();
+                }
+
+                Console.WriteLine("Would you like to start over? (yes/no)");
+                string restartChoice = Console.ReadLine();
+                if (restartChoice.Equals("no", StringComparison.OrdinalIgnoreCase))
+                {
+                    
+                    break;
+                }
+            }
+        }
+        public static void PrintFilteredGames(IEnumerable<SteamGames> filteredGames)
+        {
+            Console.WriteLine($"Found {filteredGames.Count()} games:");
+            foreach (var game in filteredGames)
+            {
+                Console.WriteLine(game);
+            }
+
+            if (!filteredGames.Any())
+            {
+                Console.WriteLine("No games found.");
+            }
+        }
+        public static double? ParseNullableDouble(string value)
+        {
+            if (double.TryParse(value, out double result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+    }
+}
+
---
+++ SteamDataLoader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace FinalProject
+{
+    public static class SteamDataLoader
+    {
+        public static List<SteamGames> LoadSteamGamesFromCSV(string filePath)
+        {
+            List<SteamGames> games = new List<SteamGames>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                   
+                    reader.ReadLine();
+
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] fields = line.Split(',');
+
+                        SteamGames game = new SteamGames
+                        {
+                            SteamID = ParseNullableInt(fields[0]),
+                            GameTitle = fields[1],
+                            ReleaseDate = ParseDateTime(fields[2]),
+                            Developer = fields[3],
+                            Publisher = fields[4],
+                            Platforms = fields[5],
+                            Categories = fields[6],
+                            Genres = fields[7].Trim(),
+                            Tags = fields[8],
+                            Achievements = ParseNullableInt(fields[9]),
+                            PositiveRatings = ParseNullableInt(fields[10]),
+                            NegativeRatings = ParseNullableInt(fields[11]),
+                            AveragePlaytime = ParseNullableInt(fields[12]),
+                            MedianPlaytime = ParseNullableInt(fields[13]),
+                            Owners = ParseNullableInt(fields[14]),
+                            Price = ParseNullableDouble(fields[15])
+                        };
+                        games.Add(game);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error loading game data from CSV: {e.Message}");
+            }
+
+            return games;
+        }
+
+        private static int? ParseNullableInt(string value)
+        {
+            if (int.TryParse(value, out int result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+        private static double? ParseNullableDouble(string value)
+        {
+            if (double.TryParse(value, out double result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+
+        private static DateTime ParseDateTime(string value)
+        {
+            if (DateTime.TryParse(value, out DateTime result))
+            {
+                return result;
+            }
+
+            return DateTime.MinValue;
+        }
+    }
+}
